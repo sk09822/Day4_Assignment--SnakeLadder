9c9f450460c4c41e902670d189f3e44d6d337a4e
sk09822 <kanadesandipan.virtuoso@gmail.com>
2021-12-19 19:20:23 +0530

    Die Rolls

---
+++ SnakeladderUC2.java
@@ -0,0 +1,8 @@
+class SnakeladderUc2 {
+public static void main(String[] Args) {
+		int start = 0;
+		System.out.println("Start Position:" +start);
+		int roll = (int) (Math.floor(Math.random() *10)%6+1);
+		System.out.println(roll);
+	}
+}
